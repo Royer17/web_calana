8c94e329ca2ac858b32647cbe96dca662806ca5a
Alvaro Sacari Alanoca <alvaro.sacari@gmail.com>
2016-03-07 12:50:10 -0500

    commit rama front nuevo layout

---
+++ application/views/inicio.php
@@ -0,0 +1,135 @@
+<div class="slider-principal">
+  <div>
+    <img src="<?php echo base_url(); ?>dist/img/sliderPrincipal/slider1_carnaval.jpg">
+    <h6 class="z-depth-1">GRAN CARNAVAL 2016: MI PUEBLO RENACE Y RECUPERA SUS COSTUMBRES</h6>
+  </div>
+  <div>
+    <img src="<?php echo(base_url()); ?>dist/img/sliderPrincipal/slider2_compactadora.png">
+    <h6>NUEVAS COMPACTADORAS ADQUIRIDAS POR LA MUNICIPALIDAD DISTRITAL CIUDAD NUEVA</h6>
+  </div>
+  <div>
+    <img src="<?php echo(base_url()); ?>dist/img/sliderPrincipal/slider3_renovarte.png">
+    <h6>CAMPAÑA VACACIONAL RENOVARTE 2016</h6>
+  </div>
+  <div>
+    <img src="<?php echo(base_url()); ?>dist/img/sliderPrincipal/slider4_simonbolivar.png">
+    <h6>REINAGURACIÓN DEL PARQUE RECREATIVO SIMON BOLÍVAR</h6>
+  </div>
+  <div>
+    <img src="<?php echo(base_url()); ?>dist/img/sliderPrincipal/slider5_izamiento.jpg">
+    <h6>IZAMIENTO DEL SAGRADO PABELLÓN NACIONAL POR EL 23° ANIVERSARIO DE CREACIÓN DEL DISTRITO DE CIUDAD NUEVA</h6>
+  </div>
+  <div>
+    <img src="<?php echo(base_url()); ?>dist/img/sliderPrincipal/slider6_paseo.jpg">
+    <h6>PASEO DE LA BANDERA POR EL 23° ANIVERSARIO DE CREACIÓN POLÍTICA DEL DISTRITO DE CIUDAD NUEVA</h6>
+  </div>
+</div>
+
+  <section class="container">
+    <div class="row">
+      <div class="col s12 m9">
+        <div class="container z-depth-2 ultimas-noticias">
+          <h3 class="titulo">ULTIMAS NOTICIAS</h3>
+          <div class="row noticia">
+            <h4 class="titulo">350 MENORES DE EDAD RECIBIRÁN DNI GRATUITO EN EL DISTRITO DE CIUDAD NUEVA</h4>
+            <div class="col s12 m4">
+              <img class="responsive-img z-depth-1 hoverable materialboxed" src="https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/1186912_1688543931433688_1292899946868305588_n.jpg?oh=0177712cd8e1b63fc19d948821c3bce5&oe=5771B076" alt="">
+            </div>
+            <div class="col s12 m8">
+              <p class="fecha"><i class="icon-calendar"></i>18 de febrero del 2016</p>
+              <p class="descripcion">Ciudad Nueva.- La municipalidad distrital de Ciudad Nueva en coordinación con el Registro Nacional de Identificación y Estado Civil (RENIEC), relaizarán este miércoles 24 de febrero de 09:00 a.m. a 1:00 p.m. la entrega de 350 DNI a todos los padres de familia que posteriormente hayan realizado el tramite gratuito documentario de sus menores hijos durante el año 2015.</p>
+              <a class="right waves-effect waves-teal" href="#!"><i class="icon-enlace left"></i>VER MÁS</a>
+            </div>
+          </div>
+
+          <div class="divider"></div>
+
+          <div class="row noticia">
+            <h4 class="titulo">350 MENORES DE EDAD RECIBIRÁN DNI GRATUITO EN EL DISTRITO DE CIUDAD NUEVA</h4>
+            <div class="col s12 m4">
+              <img class="responsive-img z-depth-1 hoverable materialboxed" src="https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/1186912_1688543931433688_1292899946868305588_n.jpg?oh=0177712cd8e1b63fc19d948821c3bce5&oe=5771B076" alt="">
+            </div>
+            <div class="col s12 m8">
+              <p class="fecha"><i class="icon-calendar"></i>18 de febrero del 2016</p>
+              <p class="descripcion">Ciudad Nueva.- La municipalidad distrital de Ciudad Nueva en coordinación con el Registro Nacional de Identificación y Estado Civil (RENIEC), relaizarán este miércoles 24 de febrero de 09:00 a.m. a 1:00 p.m. la entrega de 350 DNI a todos los padres de familia que posteriormente hayan realizado el tramite gratuito documentario de sus menores hijos durante el año 2015.</p>
+              <a class="right waves-effect waves-teal" href="#!"><i class="icon-enlace left"></i>VER MÁS</a>
+            </div>
+          </div>
+
+          <div class="divider"></div>
+
+          <div class="row noticia">
+            <h4 class="titulo">350 MENORES DE EDAD RECIBIRÁN DNI GRATUITO EN EL DISTRITO DE CIUDAD NUEVA</h4>
+            <div class="col s12 m4">
+              <img class="responsive-img z-depth-1 hoverable materialboxed" src="https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/1186912_1688543931433688_1292899946868305588_n.jpg?oh=0177712cd8e1b63fc19d948821c3bce5&oe=5771B076" alt="">
+            </div>
+            <div class="col s12 m8">
+              <p class="fecha"><i class="icon-calendar"></i>18 de febrero del 2016</p>
+              <p class="descripcion">Ciudad Nueva.- La municipalidad distrital de Ciudad Nueva en coordinación con el Registro Nacional de Identificación y Estado Civil (RENIEC), relaizarán este miércoles 24 de febrero de 09:00 a.m. a 1:00 p.m. la entrega de 350 DNI a todos los padres de familia que posteriormente hayan realizado el tramite gratuito documentario de sus menores hijos durante el año 2015.</p>
+              <a class="right waves-effect waves-teal" href="#!"><i class="icon-enlace left"></i>VER MÁS</a>
+            </div>
+          </div>
+
+          <div class="divider"></div>
+
+          <div class="row noticia">
+            <h4 class="titulo">350 MENORES DE EDAD RECIBIRÁN DNI GRATUITO EN EL DISTRITO DE CIUDAD NUEVA</h4>
+            <div class="col s12 m4 foto">
+              <img class="responsive-img z-depth-1 hoverable materialboxed" src="https://scontent-mia1-1.xx.fbcdn.net/hphotos-xfp1/v/t1.0-9/1186912_1688543931433688_1292899946868305588_n.jpg?oh=0177712cd8e1b63fc19d948821c3bce5&oe=5771B076" alt="">
+            </div>
+            <div class="col s12 m8">
+              <p class="fecha"><i class="icon-calendar"></i>18 de febrero del 2016</p>
+              <p class="descripcion">Ciudad Nueva.- La municipalidad distrital de Ciudad Nueva en coordinación con el Registro Nacional de Identificación y Estado Civil (RENIEC), relaizarán este miércoles 24 de febrero de 09:00 a.m. a 1:00 p.m. la entrega de 350 DNI a todos los padres de familia que posteriormente hayan realizado el tramite gratuito documentario de sus menores hijos durante el año 2015.</p>
+              <a class="right waves-effect waves-teal" href="#!"><i class="icon-enlace left"></i>VER MÁS</a>
+            </div>
+          </div>
+
+          <div class="divider"></div>
+
+        </div>
+      </div>
+      <div class="col s12 m3">
+
+        <div class="alcalde z-depth-2">
+          <h3 class="titulo">NUESTRO ALCALDE</h3>
+          <img class="responsive-img" src="<?php echo(base_url()); ?>dist/img/alcalde.png">
+          <a href="#!"><p class="mensaje">Estimados vecinos: Quiero expresarles mis más cordiales saludos y reiterar el sentimiento que hoy nos une, que es sacar adelante al distrito ... VER MÁS</p></a>
+          <!-- <p class="nombre">MED. EDGAR ROGELIO CONCORI COAQUIRA</p> -->
+          <p class="nombre">ING. JOSÉ GREGORIO ROQUE MAMANI</p>
+        </div>
+
+        <div class="z-depth-2 modernizacion-municipal">
+          <h3 class="titulo">MODERNIZACIÓN MUNICIPAL</h3>
+          <div class="row">
+            <div class="col s6">
+              <a class="center waves-effect waves-light hoverable boton-modernizacion" href="#!">
+                <i class="icon-tributos"></i>
+                <p>TRIBUTOS MUNICIPALES</p>
+              </a>
+            </div>
+            <div class="col s6">
+              <a class="center waves-effect waves-light hoverable boton-modernizacion" href="#!">
+                <i class="icon-tienda"></i>
+                <p>LICENCIA DE FUNCIONAMIENTO</p>
+              </a>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col s6">
+              <a class="center waves-effect waves-light hoverable boton-modernizacion" href="#!">
+                <i class="icon-edificios"></i>
+                <p>LICENCIA DE EDIFICACIONES</p>
+              </a>
+            </div>
+            <div class="col s6">
+              <a class="center waves-effect waves-light hoverable boton-modernizacion" href="#!">
+                <i class="icon-book"></i>
+                <p>LIBRO DE RECLAMACIONES</p>
+              </a>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
---
+++ application/views/layout_index/navbar.php
@@ -0,0 +1,127 @@
+<!-- Dropdown Structure -->
+<!-- navbar para pantallas moviles -->
+	<a href="#" data-activates="nav-movil" class="button-collapse"><i class="icon-menu"></i></a>
+	<ul class="side-nav" id="nav-movil">
+		<li><a href="#!">INICIO<i class="icon-home left"></i></a></li>
+		<li><a href="#!">FOTOS<i class="icon-fotos left"></i></a></li>
+		<li><a href="#!">VIDEOS<i class="icon-videos left"></i></a></li>
+		<!-- dropdown-DISTRITO-movil -->
+		<li><a class="dropdown-button" href="#!" data-activates="dropdown-DISTRITO-movil">DISTRITO<i class="icon-down right"></i></a></li>
+		<ul id="dropdown-DISTRITO-movil" class="dropdown-content">
+			<li><a href="#!">Historia del Distrito</a></li>
+		</ul>
+		<!-- dropdown-MUNICIPALIDAD-movil Trigger -->
+		<li><a class="dropdown-button" href="#!" data-activates="dropdown-MUNICIPALIDAD-movil">MUNICIPALIDAD<i class="icon-down right"></i></a></li>
+		<ul id="dropdown-MUNICIPALIDAD-movil" class="dropdown-content">
+			<li><a href="#!">Visión y Misión</a></li>
+			<li class="divider"></li>
+			<li><a href="#!">Alcalde</a></li>
+			<li><a href="#!">Consejo Municipal</a></li>
+			<li><a href="#!">Funcionarios</a></li>
+			<li><a href="#!">Comisiones</a></li>
+		</ul>
+		<!-- dropdown-OBRASYPROYECTOS-movil Trigger -->
+		<li><a class="dropdown-button" href="#!" data-activates="dropdown-OBRASYPROYECTOS-movil">OBRAS Y PROYECTOS<i class="icon-down right"></i></a></li>
+		<ul id="dropdown-OBRASYPROYECTOS-movil" class="dropdown-content">
+			<li><a href="#!">Infraestructura y obras</a></li>
+			<li><a href="#!">Desarrollo Social</a></li>
+			<li><a href="#!">Desarrollo Económico</a></li>
+		</ul>
+		<!-- dropdown-MODERNIZACION-movil Trigger -->
+		<li><a class="dropdown-button" href="#!" data-activates="dropdown-MODERNIZACION-movil">MODERNIZACIÓN<i class="icon-down right"></i></a></li>
+		<ul id="dropdown-MODERNIZACION-movil" class="dropdown-content">
+			<li><a href="#!">Tributos Municipales</a></li>
+			<li><a href="#!">Licencia de Edificaciones</a></li>
+			<li><a href="#!">Licencia de Funcionamiento</a></li>
+			<li><a href="#!">Libro de Reclamaciones</a></li>
+		</ul>
+		<!-- dropdown-SERVICIOS-movil Trigger -->
+		<li><a class="dropdown-button" href="#!" data-activates="dropdown-SERVICIOS-movil">SERVICIOS<i class="icon-down right"></i></a></li>
+		<ul id="dropdown-SERVICIOS-movil" class="dropdown-content">
+			<li><a href="#!">Seguridad Ciudadana</a></li>
+			<li><a href="#!">Vaso de Leche</a></li>
+			<li><a href="#!">DEMUNA</a></li>
+			<li><a href="#!">Deportes</a></li>
+			<li><a href="#!">Defensa Civil</a></li>
+		</ul>
+		<!-- dropdown-TRANSPARENCIA-movil Trigger -->
+		<li><a class="dropdown-button" href="#!" data-activates="dropdown-TRANSPARENCIA-movil">TRANSPARENCIA<i class="icon-down right"></i></a></li>
+		<ul id="dropdown-TRANSPARENCIA-movil" class="dropdown-content">
+			<li><a href="#!">Datos Generales</a></li>
+			<li><a href="#!">Planeamiento y Organización</a></li>
+			<li><a href="#!">Información Presupuestal</a></li>
+			<li><a href="#!">Proyectos de Inversión</a></li>
+			<li><a href="#!">Participación Ciudadana</a></li>
+			<li><a href="#!">Información del Personal</a></li>
+			<li><a href="#!">Información de Contrataciones</a></li>
+			<li class="divider"></li>
+			<li><a href="#!">Publicaciones Emitidas</a></li>
+			<li class="divider"></li>
+			<li><a href="#!">Convocatorias</a></li>
+		</ul>
+	</ul>
+
+<!-- navbar para desktop -->
+<nav>
+	<div class="nav-wrapper">
+
+		<ul class="left hide-on-med-and-down">
+
+			<li><a href="#!">INICIO<i class="icon-home left"></i></a></li>
+			<!-- dropdown-DISTRITO-desktop -->
+			<li><a class="dropdown-button" href="#!" data-activates="dropdown-DISTRITO-desktop">DISTRITO<i class="icon-down right"></i></a></li>
+			<ul id="dropdown-DISTRITO-desktop" class="dropdown-content">
+				<li><a href="#!">Historia del Distrito</a></li>
+			</ul>
+			<!-- dropdown-MUNICIPALIDAD-desktop Trigger -->
+			<li><a class="dropdown-button" href="#!" data-activates="dropdown-MUNICIPALIDAD-desktop">MUNICIPALIDAD<i class="icon-down right"></i></a></li>
+			<ul id="dropdown-MUNICIPALIDAD-desktop" class="dropdown-content">
+				<li><a href="#!">Visión y Misión</a></li>
+				<li class="divider"></li>
+				<li><a href="#!">Alcalde</a></li>
+				<li><a href="#!">Consejo Municipal</a></li>
+				<li><a href="#!">Funcionarios</a></li>
+				<li><a href="#!">Comisiones</a></li>
+			</ul>
+			<!-- dropdown-OBRASYPROYECTOS-desktop Trigger -->
+			<li><a class="dropdown-button" href="#!" data-activates="dropdown-OBRASYPROYECTOS-desktop">OBRAS Y PROYECTOS<i class="icon-down right"></i></a></li>
+			<ul id="dropdown-OBRASYPROYECTOS-desktop" class="dropdown-content">
+				<li><a href="#!">Infraestructura y obras</a></li>
+				<li><a href="#!">Desarrollo Social</a></li>
+				<li><a href="#!">Desarrollo Económico</a></li>
+			</ul>
+			<!-- dropdown-MODERNIZACION-desktop Trigger -->
+			<li><a class="dropdown-button" href="#!" data-activates="dropdown-MODERNIZACION-desktop">MODERNIZACIÓN<i class="icon-down right"></i></a></li>
+			<ul id="dropdown-MODERNIZACION-desktop" class="dropdown-content">
+				<li><a href="#!">Tributos Municipales</a></li>
+				<li><a href="#!">Licencia de Edificaciones</a></li>
+				<li><a href="#!">Licencia de Funcionamiento</a></li>
+				<li><a href="#!">Libro de Reclamaciones</a></li>
+			</ul>
+			<!-- dropdown-SERVICIOS-desktop Trigger -->
+			<li><a class="dropdown-button" href="#!" data-activates="dropdown-SERVICIOS-desktop">SERVICIOS<i class="icon-down right"></i></a></li>
+			<ul id="dropdown-SERVICIOS-desktop" class="dropdown-content">
+				<li><a href="#!">Seguridad Ciudadana</a></li>
+				<li><a href="#!">Vaso de Leche</a></li>
+				<li><a href="#!">DEMUNA</a></li>
+				<li><a href="#!">Deportes</a></li>
+				<li><a href="#!">Defensa Civil</a></li>
+			</ul>
+			<!-- dropdown-TRANSPARENCIA-desktop Trigger -->
+			<li><a class="dropdown-button" href="#!" data-activates="dropdown-TRANSPARENCIA-desktop">TRANSPARENCIA<i class="icon-down right"></i></a></li>
+			<ul id="dropdown-TRANSPARENCIA-desktop" class="dropdown-content">
+				<li><a href="#!">Datos Generales</a></li>
+				<li><a href="#!">Planeamiento y Organización</a></li>
+				<li><a href="#!">Información Presupuestal</a></li>
+				<li><a href="#!">Proyectos de Inversión</a></li>
+				<li><a href="#!">Participación Ciudadana</a></li>
+				<li><a href="#!">Información del Personal</a></li>
+				<li><a href="#!">Información de Contrataciones</a></li>
+				<li class="divider"></li>
+				<li><a href="#!">Publicaciones Emitidas</a></li>
+				<li class="divider"></li>
+				<li><a href="#!">Convocatorias</a></li>
+			</ul>
+		</ul>
+	</div>
+</nav>
